f99d0bddf86ada3784e463d952afede56f0073c4
Ashwin Jose <aporuthu@asu.edu>
2020-01-29 21:48:10 -0700

    Bootstrap and basic theming

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
 function scrollOnClick(id) {
   console.log("Inside scroller function");
-  let elt = document.getElementById(id.id);
-  elt.scrollIntoView({behavior:"smooth"});
+  var element = document.getElementById(id.id);
+  var headerOffset = 65;
+  const bodyRect = document.body.getBoundingClientRect().top;
+  const elementRect = element.getBoundingClientRect().top;
+  const elementPosition = elementRect - bodyRect;
+  const offsetPosition = elementPosition - headerOffset;
 
+  window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth"
+   });
 }
