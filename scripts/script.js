2bd633857949759228d8e9f03ca7b6ad172e01db
Ashwin Jose <aporuthu@asu.edu>
2020-01-30 10:47:18 -0700

    scroll fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,28 @@
 function scrollOnClick(id) {
   console.log("Inside scroller function");
   var element = document.getElementById(id.id);
-  var headerOffset = 65;
+  var screenWidth = screen.width;
+  var headerOffset = 250;
+  if(screenWidth>597)
+  {
+    headerOffset = 40;
+  }
+  else if (screenWidth>321) {
+    headerOffset =70;
+  }
+  else if (screenWidth>290) {
+    headerOffset =100;
+  }
+  else if (screenWidth>215) {
+    headerOffset =140;
+  }
+  else if (screenWidth>183) {
+    headerOffset =190;
+  }
+  else if (screenWidth>156) {
+    headerOffset =220;
+  }
+
   const bodyRect = document.body.getBoundingClientRect().top;
   const elementRect = element.getBoundingClientRect().top;
   const elementPosition = elementRect - bodyRect;
