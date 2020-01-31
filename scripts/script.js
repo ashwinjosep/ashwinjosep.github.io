0cdf94162d93722296cdcf88bc30bed6707026fe
Ashwin Jose <aporuthu@asu.edu>
2020-01-30 18:59:45 -0700

    adding hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,27 @@
 function scrollOnClick(id) {
   console.log("Inside scroller function");
   var element = document.getElementById(id.id);
-  var screenWidth = screen.width;
-  var headerOffset = 250;
-  if(screenWidth>597)
-  {
-    headerOffset = 40;
-  }
-  else if (screenWidth>321) {
-    headerOffset =70;
-  }
-  else if (screenWidth>290) {
-    headerOffset =100;
-  }
-  else if (screenWidth>215) {
-    headerOffset =140;
-  }
-  else if (screenWidth>183) {
-    headerOffset =190;
-  }
-  else if (screenWidth>156) {
-    headerOffset =220;
-  }
+  // var screenWidth = screen.width;
+  var headerOffset = 40;
+  // if(screenWidth>597)
+  // {
+  //   headerOffset = 40;
+  // }
+  // else if (screenWidth>321) {
+  //   headerOffset =70;
+  // }
+  // else if (screenWidth>290) {
+  //   headerOffset =100;
+  // }
+  // else if (screenWidth>215) {
+  //   headerOffset =140;
+  // }
+  // else if (screenWidth>183) {
+  //   headerOffset =190;
+  // }
+  // else if (screenWidth>156) {
+  //   headerOffset =220;
+  // }
 
   const bodyRect = document.body.getBoundingClientRect().top;
   const elementRect = element.getBoundingClientRect().top;
@@ -32,4 +32,8 @@ function scrollOnClick(id) {
       top: offsetPosition,
       behavior: "smooth"
    });
+}
+
+function toggleHamburgerLinks() {
+
 }
